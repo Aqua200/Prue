fc39309fd13ce26358520d87f67a540f96923f35
Aqua200 <chinquepapa@gmail.com>
2024-11-20 22:56:00 -0800

    Update Config•simi.js

--- plugins/Config•simi.js
+++ plugins/Config•simi.js
@@ -1,38 +1,60 @@
+import translate from '@vitalets/google-translate-api'
 import fetch from 'node-fetch'
-
-var handler = (m) => m
-handler.before = async (m) => {
-
-let chat = global.db.data.chats[m.chat]
-if (chat.simi) {
-if (/^.*false|disnable|(turn)?off|0/i.test(m.text)) return
-let textodem = m.text
-
+let handler = async (m, {conn, text, usedPrefix, command }) => {
+let resp
+let consola
+if (resp == undefined) {resp = `Stop genera: ${consola}`}
+if (!text) {
+resp = `*[❗] INGRESE UN TEXTO PARA HABLAR CON SIMSIMI O EL BOT*\n\n*EJEMPLO: ${usedPrefix + command} Hola bot*`
+} else {
 try {
-
-await conn.sendPresenceUpdate('composing', m.chat)
-let ressimi = await fetch(`https://api.simsimi.net/v2/?text=${encodeURIComponent(textodem)}&lc=es`)
-let data = await ressimi.json();
-if (data.success == 'No s\u00e9 lo qu\u00e9 est\u00e1s diciendo. Por favor ense\u00f1ame.') return conn.reply(m.chat, `${lol}`, m, fake, ) /* EL TEXTO "lol" NO ESTA DEFINIDO PARA DAR ERROR Y USAR LA OTRA API */
-await m.reply(data.success)
+let res = await fetch(`https://api.simsimi.net/v2/?text=${text}&lc=es`)
+let json = await res.json()
+resp = json.success.trim()
 } catch {
-      /* SI DA ERROR USARA ESTA OTRA OPCION DE API DE IA QUE RECUERDA EL NOMBRE DE LA PERSONA */
-if (textodem.includes('Hola')) textodem = textodem.replace('Hola', 'Hello');
-if (textodem.includes('hola')) textodem = textodem.replace('hola', 'hello');
-if (textodem.includes('HOLA')) textodem = textodem.replace('HOLA', 'HELLO');
-let reis = await fetch('https://translate.googleapis.com/translate_a/single?client=gtx&sl=auto&tl=en&dt=t&q=' + textodem)
+try {
+if (text.includes('Hola')) text = text.replace('Hola', 'Hello')
+if (text.includes('hola')) text = text.replace('hola', 'Hello')
+if (text.includes('HOLA')) text = text.replace('HOLA', 'HELLO')
+let reis = await fetch("https://translate.googleapis.com/translate_a/single?client=gtx&sl=auto&tl=en&dt=t&q=" + text)
 let resu = await reis.json()
 let nama = m.pushName || '1'
-let api = await fetch('http://api.brainshop.ai/get?bid=153868&key=rcKonOgrUFmn5usX&uid=' + nama + '&msg=' + resu[0][0][0])
+let api = await fetch("http://api.brainshop.ai/get?bid=153868&key=rcKonOgrUFmn5usX&uid=" + nama + "&msg=" + resu[0][0][0])
 let res = await api.json()
-let reis2 = await fetch('https://translate.googleapis.com/translate_a/single?client=gtx&sl=auto&tl=es&dt=t&q=' + res.cnt)
+let reis2 = await fetch("https://translate.googleapis.com/translate_a/single?client=gtx&sl=auto&tl=es&dt=t&q=" + res.cnt)
 let resu2 = await reis2.json()
-await conn.reply(m.chat, resu2[0][0][0], m, fake, )
+console.log('bot: ', resu2)
+if (!resu2[0][0]) return
+resp = resu2[0][0][0]
+} catch (e) {
+console.log(`este es el error del codigo anterior: ${e}`)
+/*let res2 = await fetch(`https://api.phamvandien.xyz/sim?type=ask&ask=${text}`)
+let json2 = await res2.json()
+let result = json2.answer
+let reisss = await fetch("https://translate.googleapis.com/translate_a/single?client=gtx&sl=auto&tl=id&dt=t&q=" + text)
+let resuuu = await reisss?.json()
+let res2 = await fetch(`https://violetics.pw/api/utility/simsimi?apikey=beta&text=${resuuu[0][0][0]}`)
+let json2 = await res2.json()
+let result = json2.result
+let lol = await translate(`${result}`, { to: 'es', autoCorrect: true })
+resp = lol.text.trim()*/
+}
+}
+}
+let txt = '';
+let count = 0;
+for (const c of resp) {
+await new Promise(resolve => setTimeout(resolve, 1));
+txt += c;
+count++;
+if (count % 10 === 0) {
+await conn.sendPresenceUpdate('composing' , m.chat);
+}
+}
 
+return conn.sendMessage(m.chat, { text: txt.trim(), mentions: conn.parseMention(txt) }, {quoted: m, ephemeralExpiration: 24*60*100, disappearingMessagesInChat: 24*60*100})
 }
-return !0
-}
-return true
-
-}
+handler.help = ['simi', 'bot'].map(v => v + ' <teks>')
+handler.tags = ['fun']
+handler.command = /^((sim)?simi|bot|alexa|cortana)$/i
 export default handler
